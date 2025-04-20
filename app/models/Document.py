f30e06dbb5c13de4c35ec457a92d73152b864484
flyBoy667 <salifoudembele100@outlook.com>
2025-04-20 00:23:02 +0000

    Ajout des modèles Document et DocType avec leurs attributs et relations dans le fichier Document.py.

--- app/models/Document.py
+++ app/models/Document.py
@@ -1,2 +1,33 @@
-class Document:
-    pass
+from app.conf import db
+
+
+class DocType(db.Model):
+    __tablename__ = "doc_type"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp(),
+    )
+
+
+class Document(db.Model):
+    __tablename__ = "document"
+    doc_id = db.Column(db.Integer, primary_key=True, unique=True)
+    doc_name = db.Column(db.String(200))
+    doc_content = db.Column(db.String(1000))
+    doc_type_id = db.Column(db.Integer, db.ForeignKey("doc_type.id"))
+    doc_type = db.relationship("DocType", backref="documents")
+    doc_format = db.Column(db.String(1000))
+
+    doc_insert_date = db.Column(db.DateTime, default=db.func.current_timestamp())
+    doc_updated_date = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp(),
+    )
+
+    doc_file_full_path = db.Column(db.String(1000))
